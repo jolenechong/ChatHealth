7efb1da1edf274c63a28eca541eeeb47e64de7b3
Jolene <jolenechong7@gmail.com>
2021-09-04 18:20:28 +0800

    package.json, server.js and procfile files upload

---
+++ server.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//run when client connects
+const botName = "ChatHealth Admin"
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+      const user = userJoin(socket.id, username, room);
+  
+      socket.join(user.room);
+  
+      // welcome current user
+      socket.emit('message', formatMessage(botName, 'Welcome to ChatHealth!'));
+  
+      // broadcast when a user connects
+      socket.broadcast
+        .to(user.room)
+        .emit(
+          'message',
+          formatMessage(botName, `${user.username} has joined the chat`)
+        );
+  
+      // send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    });
+  
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+      const user = getCurrentUser(socket.id);
+  
+      io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+  
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+      const user = userLeave(socket.id);
+  
+      if (user) {
+        io.to(user.room).emit(
+          'message',
+          formatMessage(botName, `${user.username} has left the chat`)
+        );
+  
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+          room: user.room,
+          users: getRoomUsers(user.room)
+        });
+      }
+    });
+  });
+
+const PORT = process.env.PORT || 3000;
+// processs.env.POST allows us to post on heroku for eg
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
